e1e07c67275fc9c9bb9256b58f84b71c53b49f7c
Krista Pratico <krpratic@microsoft.com>
2023-06-16 12:05:07 -0700

    more tests

--- openai/tests/test_client.py
+++ openai/tests/test_client.py
@@ -14,6 +14,8 @@ COMPLETION_MODEL = "text-davinci-003"
 CHAT_COMPLETION_MODEL = "gpt-35-turbo"
 CHAT_COMPLETION_MODEL_OPENAI = "gpt-3.5-turbo"
 EMBEDDINGS_MODEL = "text-embedding-ada-002"
+IMAGE_PATH = ""
+MASK_IMAGE_PATH = ""
 
 
 def configure_client(f):
@@ -58,7 +60,31 @@ def clear_oai_module(monkeypatch: pytest.MonkeyPatch):
 def setup_oai_module(monkeypatch: pytest.MonkeyPatch, **kwargs):
     for n, v in kwargs.items():
         monkeypatch.setattr(openai, n, v)
-        
+
+@pytest.fixture
+def embedding_response():
+    return   {
+                "object": "list",
+                "data": [
+                    {
+                    "object": "embedding",
+                    "embedding": [
+                        0.0023064255,
+                        -0.009327292,
+                        -0.0028842222,
+                    ],
+                    "index": 0
+                    }
+                ],
+                "model": "text-embedding-ada-002",
+                "usage": {
+                    "prompt_tokens": 8,
+                    "total_tokens": 8
+                }
+        }
+
+
+# MOCK TESTS ------------------------------------------------
 def test_construct_client(monkeypatch: pytest.MonkeyPatch, clear_oai_module):
     setup_oai_module(monkeypatch, api_key=None)
     client = openai.client.OpenAIClient()
@@ -98,28 +124,6 @@ def test_construct_openai_client_api_key():
     assert client.api_base == openai.api_base
     assert client.api_type == 'open_ai'
     assert client.auth.get_token() == 'secret key'
-
-@pytest.fixture
-def embedding_response():
-    return   {
-                "object": "list",
-                "data": [
-                    {
-                    "object": "embedding",
-                    "embedding": [
-                        0.0023064255,
-                        -0.009327292,
-                        -0.0028842222,
-                    ],
-                    "index": 0
-                    }
-                ],
-                "model": "text-embedding-ada-002",
-                "usage": {
-                    "prompt_tokens": 8,
-                    "total_tokens": 8
-                }
-        }
 
 def test_make_call_client_aad(monkeypatch: pytest.MonkeyPatch, clear_oai_module, embedding_response):
     provided_api_base = 'https://contoso.microsoft.com'
@@ -174,6 +178,108 @@ def test_make_call_client_oai_key(monkeypatch: pytest.MonkeyPatch, clear_oai_mod
     client = openai.client.OpenAIClient(auth="secret key")
     client.embeddings("some data", model='das model')
 
+
+def test_populate_args():
+    client = openai.client.OpenAIClient()
+
+    # valid override
+    kwargs = {
+        "api_base": "expected",
+        "api_key": "expected",
+        "api_version": "expected",
+        "prompt": "expected",
+    }
+
+    overrides = {
+        "temperature": 0.1
+    }
+
+    client._populate_args(kwargs, **overrides)
+
+    assert kwargs == {
+        "api_base": "expected",
+        "api_key": "expected",
+        "api_type": "open_ai",
+        "api_version": "expected",
+        "prompt": "expected",
+        "temperature": 0.1
+    }
+
+
+    # unexpected override by user
+    kwargs = {
+        "prompt": "expected",
+        "api_base": "expected",
+        "api_key": "expected",
+        "api_type": "expected",
+        "api_version": "expected",
+        "stream": True
+    }
+
+    overrides = {
+        "stream": False
+    }
+
+    with pytest.raises(TypeError):
+        client._populate_args(kwargs, **overrides)
+
+    # attempt to change api_base on per-method call
+    kwargs = {
+        "prompt": "expected",
+        "api_base": "expected",
+        "api_key": "expected",
+        "api_type": "expected",
+        "api_version": "expected",
+        "stream": True
+    }
+
+    overrides = {
+        "api_base": "update",
+    }
+
+    with pytest.raises(TypeError):
+        client._populate_args(kwargs, **overrides)
+
+
+def test_normalize_model():
+    client = openai.client.OpenAIClient(backend="azure", api_base="azurebase")
+
+    # azure: deployment_id --> deployment_id
+    kwargs = {"deployment_id": "ada"}
+    client._normalize_model(kwargs)
+    assert kwargs == {"deployment_id": "ada"}
+
+    # azure: engine --> engine
+    kwargs = {"engine": "ada"}
+    client._normalize_model(kwargs)
+    assert kwargs == {"engine": "ada"}
+
+    # azure: model --> deployment_id (normalized)
+    kwargs = {"model": "ada"}
+    client._normalize_model(kwargs)
+    assert kwargs == {"deployment_id": "ada"}
+
+    client = openai.client.OpenAIClient(backend="openai")
+    # openai: deployment_id --> model
+    kwargs = {"deployment_id": "ada"}
+    client._normalize_model(kwargs)
+    # incorrect arg raised by library
+    assert kwargs == {"deployment_id": "ada"}
+
+    # openai: engine --> engine
+    kwargs = {"engine": "ada"}
+    client._normalize_model(kwargs)
+    assert kwargs == {"engine": "ada"}
+
+    # openai: model --> model
+    kwargs = {"model": "ada"}
+    client._normalize_model(kwargs)
+    assert kwargs == {"model": "ada"}
+
+    # too many args
+    kwargs = {"model": "ada", "deployment_id": "ada"}
+    with pytest.raises(TypeError):
+        client._normalize_model(kwargs)
 
 # LIVE TESTS ------------------------------------------------
 # COMPLETION TESTS
@@ -310,7 +416,7 @@ async def test_client_aembeddings(api_type, **kwargs):
 def test_client_image_create(api_type, **kwargs):
     client = kwargs.pop("client")
     image = client.image(
-        prompt="a cat yowling for food",
+        prompt="A cute baby sea otter",
         n=1
     )
     assert image
@@ -321,7 +427,60 @@ def test_client_image_create(api_type, **kwargs):
 async def test_client_aimage_create(api_type, **kwargs):
     client = kwargs.pop("client")
     image = await client.aimage(
-        prompt="a cat yowling for food",
+        prompt="A cute baby sea otter",
         n=1
     )
     assert image
+
+
+# IMAGE VARIATION TESTS
+@pytest.mark.parametrize("api_type", ["openai"])
+@configure_client
+def test_client_image_variation(api_type, **kwargs):
+    client = kwargs.pop("client")
+    variation = client.image_variation(
+        image=open(IMAGE_PATH, "rb"),
+        n=2,
+        size="1024x1024"
+    )
+    assert variation
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("api_type", ["openai"])
+@configure_client
+async def test_client_aimage_variation(api_type, **kwargs):
+    client = kwargs.pop("client")
+    variation = await client.aimage_variation(
+        image=open(IMAGE_PATH, "rb"),
+        n=2,
+        size="1024x1024"
+    )
+    assert variation
+
+# IMAGE EDIT TESTS
+@pytest.mark.parametrize("api_type", ["openai"])
+@configure_client
+def test_client_image_edit(api_type, **kwargs):
+    client = kwargs.pop("client")
+    edit = client.image_edit(
+        image=open(IMAGE_PATH, "rb"),
+        mask=open(MASK_IMAGE_PATH, "rb"),
+        prompt="A cute baby sea otter wearing a beret",
+        n=2,
+        size="1024x1024"
+    )
+    assert edit
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("api_type", ["openai"])
+@configure_client
+async def test_client_aimage_edit(api_type, **kwargs):
+    client = kwargs.pop("client")
+    edit = await client.aimage_edit(
+        image=open(IMAGE_PATH, "rb"),
+        mask=open(MASK_IMAGE_PATH, "rb"),
+        prompt="A cute baby sea otter wearing a beret",
+        n=2,
+        size="1024x1024"
+    )
+    assert edit
