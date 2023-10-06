a26725f06bd848697237c4c7bab6addba6dde9ce
Krista Pratico <krpratic@microsoft.com>
2023-10-05 19:17:04 -0700

    try custom auth

--- src/openai/azure/_credential.py
+++ src/openai/azure/_credential.py
@@ -1,3 +1,8 @@
+from typing import AsyncGenerator, Generator, Any
+import asyncio
+import httpx
+
+
 class TokenCredential:
     """Placeholder/example token credential class
     
@@ -11,3 +16,25 @@ class TokenCredential:
     def get_token(self):
         return self._credential.get_token('https://cognitiveservices.azure.com/.default').token
 
+
+class TokenAuth(httpx.Auth):
+    def __init__(self, credential: "TokenCredential") -> None:
+        self._credential = credential
+        self._async_lock = asyncio.Lock()
+
+    def sync_get_token(self) -> str:
+        return self._credential.get_token('https://cognitiveservices.azure.com/.default').token
+
+    def sync_auth_flow(self, request: httpx.Request) -> Generator[httpx.Request, Any, Any]:
+        token = self.sync_get_token()
+        request.headers["Authorization"] = f"Bearer {token}"
+        yield request
+
+    async def async_get_token(self) -> str:
+        async with self._async_lock:
+            return await self._credential.get_token('https://cognitiveservices.azure.com/.default').token
+
+    async def async_auth_flow(self, request: httpx.Request) -> AsyncGenerator[httpx.Request, Any, Any]:
+        token = await self.async_get_token()
+        request.headers["Authorization"] = f"Bearer {token}"
+        yield request
