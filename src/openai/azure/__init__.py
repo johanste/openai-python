d25a1db62442ae93f6354b31f853a91dedb5d780
Krista Pratico <krpratic@microsoft.com>
2023-10-16 16:03:06 -0700

    add to init

--- src/openai/azure/__init__.py
+++ src/openai/azure/__init__.py
@@ -1,9 +1,12 @@
 from ._sync_client import AzureOpenAIClient
 from ._async_client import AsyncAzureOpenAIClient
 from ._credential import TokenCredential
+from ._streaming import AzureStream, AzureAsyncStream
 
 __all__ = [
     "AzureOpenAIClient",
     "TokenCredential",
     "AsyncAzureOpenAIClient",
+    "AzureStream",
+    "AzureAsyncStream",
 ]
